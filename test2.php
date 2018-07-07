ecd9ad4d3dc5d734f4291a69e74e97f023b51c6c
lightWey <lightWay.zhi@gmail.com>
2018-07-07 18:34:03 +0800

    Update test2.php

--- test2.php
+++ test2.php
@@ -1,2 +1,4 @@
-<?php
-echo 123;
+- @mentions, #refs, [links](), **formatting**, and <del>tags</del> supported
+- list syntax required (any unordered or ordered list supported)
+- this is a complete item
+- this is an incomplete item
