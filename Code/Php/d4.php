68483f1a6b872a42065d93b47f33ee94fb7bae5e
bocai <bocai@ibantang.com>
2019-05-31 19:03:50 +0800

    test

--- Code/Php/d4.php
+++ Code/Php/d4.php
@@ -1,19 +1,37 @@
 <?php
-$a = [];
-$b = null;
-exec('git remote -v', $a, $b);
+$p = getcwd();
+var_dump($p);exit();
 
-if ($b) {
-	echo 'error !';
+print_r($p);
+$config = [
+	'github.com'  => 'https://raw.githubusercontent.com/{{name}}/{{notebook}}/{{branch}}/{{path}}',
+    'git.dev.tencent.com' => 'https://dev.tencent.com/u/{{name}}/p/{{notebook}}/git/raw/{{branch}}/{{path}}'
+];
+
+$a = $b = null;
+exec('type git', $a, $b);
+
+if ($b || empty($a)) {
+	echo 'not found git command !';
 	return flase;
 }
 
-if (empty($a)) {
-	echo 'empty !';
-	return false;
-}
+$a=[];
+$b=0;
+exec('git remote -v', $a, $b);
 
+$pattern = '/^origin\s(https?:\/\/(.+\.com).*\.git).*\(push\)/';
+
+$cc = [];
 
 foreach ($a as $v) {
-	echo $v;
+	$c1 = [];
+	if (preg_match($pattern, $v, $c1) && isset($c1[2])) {
+		$cc[] = $c1[2];
+	}
 }
+
+echo PHP_EOL;
+echo DIRECTORY_SEPARATOR;
+
+print_r(scandir($p));
