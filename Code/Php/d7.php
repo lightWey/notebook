9b75a5295f575ba9942d51fbcd4346b73f5a6be7
bocai <bocai@ibantang.com>
2019-08-24 10:04:21 +0800

    缓存机制优化

--- Code/Php/d7.php
+++ Code/Php/d7.php
@@ -1,1 +1,39 @@
 <?php
+
+class Solution
+{
+    /**
+     * @param String $s
+     * @return String
+     */
+    public function reverseWords($s)
+    {
+        $delimiter = ' ';
+        $arr = explode($delimiter, $s);
+        $str = '';
+
+        $count = count($arr) - 1;
+        for ($i = $count; $i >= 0; $i--) {
+            $str .= empty($arr[$i]) ? '' : $delimiter.$arr[$i];
+        }
+        return trim($str);
+    }
+
+    public function reverseWords1($s)
+    {
+        $delimiter = ' ';
+        return implode($delimiter, array_reverse(array_filter(explode($delimiter, $s))));
+    }
+
+    public function reverseWords2($s)
+    {
+        $word  = $delimiter = [];
+        $count = count($s) - 1;
+        for ($i = $count; $i <= 0; $i--) {
+
+        }
+    }
+}
+
+$a = new Solution();
+echo $a->reverseWords1("    hello    world!  ");
